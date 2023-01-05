8ba5295344f29e0ed1067ead0956d877708868ee
inspector123 <7421886101a@gmail.com>
2023-01-05 11:59:00 -0800

    add

--- index.js
+++ index.js
@@ -31,11 +31,12 @@ app.listen(PORT, () => {
  console.log(`server running on port ${PORT}`);
 });
 
-const localNodeIp = "192.168.0.228"
+const fullNodeIp = "192.168.0.228"
+const archiveNodeIp = "192.168.0.155"
 const httpPort = "9535"
 const wssPort = "9536"
 
-const httpUrl = `http://${localNodeIp}:${httpPort}`
-const wsUrl = `ws://${localNodeIp}:${wssPort}`
+const fullNodeUrl = `http://${fullNodeIp}:${httpPort}`
+const archiveNodeUrl = `http://${archiveNodeIp}:${httpPort}`
 
-const watcher = new ContractWatcher(CHAT_ID_CHANNEL_BETA, VOLUME_BOT_KEY, httpUrl, wsUrl);
+const watcher = new ContractWatcher(CHAT_ID_CHANNEL_BETA, VOLUME_BOT_KEY, fullNodeUrl, archiveNodeUrl);
