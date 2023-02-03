816240bd2d5ea3ad839bf2b89e154f9e67d2ebfb
inspector123 <7421886101a@gmail.com>
2023-02-03 15:34:38 -0800

    latest -- new TG bots, two channels.. next is fresh wallet

--- LatestBlockWatcher.js
+++ LatestBlockWatcher.js
@@ -58,90 +58,119 @@ export class LatestBlockWatcher {
     }
 
     async latestBlockWallets(block) {
-        const response = await api.get('/api/swaps/1?max=true')
-        const max = response.data.data[0].maxBlockNumber;
-        console.log(max)
-        if (max == block) return;
-        let swaps = await this.archiveProvider.getLogs({topics:[[v2topic,v3topic]], fromBlock: block});
-        this.swapParser.reset();
-        this.swapParser.getAllPairs();
-        for (let i in swaps) {
-            await this.swapParser.grabSwap(swaps[i]);
-        }
-        if (this.swapParser.newPairsData.length) {
-            try {
-                await api.post('/api/pairs', this.swapParser.newPairsData)
-            }catch(e) {
-                console.log(e.response.data)
-            }
-        }
-        if (this.swapParser.allSwapsData.length) {
-            try {
-                await api.post(`/api/swaps?table=MainSwaps`, this.swapParser.allSwapsData)
-            }catch(e) {
-                console.log(e.response.data)
-            }
-        }
-        let { allSwapsData: _swaps } = this.swapParser;
-        for (let i in _swaps) {
-            const checkForWallet = wallets.filter(w=>w.wallet==_swaps[i].wallet);
-            if (checkForWallet.length) {
-                console.log(checkForWallet[0])
+        try {
+            const response = await api.get('/api/swaps/1?max=true')
+            const max = response.data.data[0].maxBlockNumber;
+            console.log(max)
+            if (max == block) return;
 
+            //Get swaps for current block
+            let swaps = await this.archiveProvider.getLogs({topics:[[v2topic,v3topic]], fromBlock: block});
+            this.swapParser.reset();
+            this.swapParser.getAllPairs();
+            for (let i in swaps) {
+                await this.swapParser.grabSwap(swaps[i]);
+            }
+
+            //Post to pairs db
+            if (this.swapParser.newPairsData.length) {
                 try {
-                    this.alertBot.telegram.sendMessage(this.chatId, 
-                        `${_swaps[i].isBuy ? `Bought ` : `Sold`} $${`${_swaps[i].usdVolume}`.replace(/\.[0-9]*/, "")} ${_swaps[i].symbol}
+                    await api.post('/api/pairs', this.swapParser.newPairsData)
+                }catch(e) {
+                    console.log(e.response.data)
+                }
+            }
+
+            //Post to MainSwaps db
+            if (this.swapParser.allSwapsData.length) {
+                try {
+                    await api.post(`/api/swaps?table=MainSwaps`, this.swapParser.allSwapsData)
+                }catch(e) {
+                    console.log(e.response.data)
+                }
+            }
+
+            //Run EpiWallet Routine
+            let { allSwapsData: _swaps } = this.swapParser;
+            for (let i in _swaps) {
+                const checkForWallet = wallets.filter(w=>w.wallet==_swaps[i].wallet);
+                if (checkForWallet.length) {
+                    console.log(checkForWallet, _swaps[i].wallet)
+                    
+                    try {
+                        this.alertBot.telegram.sendMessage(this.chatId, 
+                            `${_swaps[i].isBuy ? `Bought ` : `Sold`} $${`${_swaps[i].usdVolume}`.replace(/\.[0-9]*/, "")} ${_swaps[i].symbol}
+        New transaction from ${_swaps[i].wallet} on ${_swaps[i].router}
+
+        contract address: https://etherscan.io/token/${_swaps[i].contract}
+
+        wallet: https://etherscan.io/address/${_swaps[i].wallet}
+
+        MARKETCAP: $${`${_swaps[i].marketCap}`.replace(/\.[0-9]*/, "")}
+
+        CHART: https://dextools.io/app/ether/pair-explorer/${_swaps[i].pairAddress}
+        This wallet interacted ${checkForWallet[0].interactions} of the last 6 times the bot was active.
+                        `).catch(e=>console.log(e))
+                    }
+                    catch(e) {
+                        console.log(e, 'dfkjladfkjlfds')
+                    }
+                }
+                if (this.swapParser.allSwapsData.length) {
+                    try {
+                        await api.post(`/api/swaps?table=EpiWalletSwaps`, this.swapParser.allSwapsData)
+                    }catch(e) {
+                        console.log(e.response.data)
+                    }
+                }
+            }
+            for (let i in _swaps) {
+                const checkForWallet = walletsUnfiltered.filter(w=>w.wallet==_swaps[i].wallet);
+                if (checkForWallet.length) {
+                    console.log(checkForWallet[0], _swaps[i].wallet)
+                    const count = await api.get(`/api/swaps?table=EpiWalletSwapsUnfiltered&wallet="${checkForWallet[0].wallet}"&contract="${_swaps[i].contract}"`)
+                    const countWallet = await api.get(`/api/swaps?table=EpiWalletSwapsUnfiltered&wallet="${checkForWallet[0].wallet}"`)
+                    const countContract =  await api.get(`/api/swaps?table=EpiWalletSwapsUnfiltered&contract="${_swaps[i].contract}"`)
+                    //console.log(count.data.data[0], countWallet.data, countContract.data.data[0])
+                    console.log(count.data)
+                    
+                    
+                    this.alertBot.telegram.sendMessage(this.chatIdUnfiltered, 
+                        `${_swaps[i].isBuy ? `Bought ` : `Sold`} $${`${_swaps[i].usdVolume}`.replace(/\.[0-9]*/, "")}  ${_swaps[i].symbol}
     New transaction from ${_swaps[i].wallet} on ${_swaps[i].router}
 
     contract address: https://etherscan.io/token/${_swaps[i].contract}
-
-    wallet: https://etherscan.io/address/${_swaps[i].wallet}
 
     MARKETCAP: $${`${_swaps[i].marketCap}`.replace(/\.[0-9]*/, "")}
 
     CHART: https://dextools.io/app/ether/pair-explorer/${_swaps[i].pairAddress}
     This wallet interacted ${checkForWallet[0].interactions} of the last 6 times the bot was active.
-                    `).catch(e=>console.log(e))
-                }
-                catch(e) {
-                    console.log(e, 'dfkjladfkjlfds')
+    Count on this wallet: ${count ? count.data.data[0] : 0}
+    
+    Count on this contract:${countContract.data ? countContract.data.data[0].count : 0}
+                    `).catch(e=>console.log(e, 'fdaskjlfdaskj'))
+                   // send to EpiWalletsUnfiltered dtabase
+                    if (this.swapParser.allSwapsData.length) {
+                        try {
+                            await api.post(`/api/swaps?table=EpiWalletSwapsUnfiltered`, this.swapParser.allSwapsData)
+                        }catch(e) {
+                            console.log(e.response.data)
+                        }
+                    }
                 }
             }
-            if (this.swapParser.allSwapsData.length) {
-                try {
-                    await api.post(`/api/swaps?table=EpiWalletSwaps`, this.swapParser.allSwapsData)
-                }catch(e) {
-                    console.log(e.response.data)
-                }
-            }
+
+
+
+            //Run FreshWallet Routine
+
+
+
+
+            this.swapParser.reset();
+        }catch(e) {
+            console.log(e)
         }
-        for (let i in _swaps) {
-            const checkForWallet = walletsUnfiltered.filter(w=>w.wallet==_swaps[i].wallet);
-            if (checkForWallet.length) {
-                console.log(checkForWallet[0])
-                this.alertBot.telegram.sendMessage(this.chatIdUnfiltered, 
-                    `${_swaps[i].isBuy ? `Bought ` : `Sold`} $${`${_swaps[i].usdVolume}`.replace(/\.[0-9]*/, "")}  ${_swaps[i].symbol}
-New transaction from ${_swaps[i].wallet} on ${_swaps[i].router}
-
-contract address: https://etherscan.io/token/${_swaps[i].contract}
-
-MARKETCAP: $${`${_swaps[i].marketCap}`.replace(/\.[0-9]*/, "")}
-
-CHART: https://dextools.io/app/ether/pair-explorer/${_swaps[i].pairAddress}
-This wallet interacted ${checkForWallet[0].interactions} of the last 6 times the bot was active.
-                `).catch(e=>console.log(e, 'fdaskjlfdaskj'))
-                //send to EpiWallets dtabase
-                // if (this.swapParser.allSwapsData.length) {
-                //     try {
-                //         await api.post(`/api/swaps?table="EpiWalletSwaps"`, this.swapParser.allSwapsData)
-                //     }catch(e) {
-                //         console.log(e.response.data)
-                //     }
-                // }
-            }
-        }
-
-        this.swapParser.reset();
 
 
     }
